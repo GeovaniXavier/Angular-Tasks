afe8085b51eabc44f818eddb566c0398360db456
GeovaniX <geovanisantos019@gmail.com>
2023-07-20 20:02:01 -0300

    Adicionando itens ao projeto tasks

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public taskList: Array<TaskList> = [
+    {task: "Minha nova Task", checked: true},
+    {task: "Minha nova Task 2", checked: false}
+  ];
 
 }
